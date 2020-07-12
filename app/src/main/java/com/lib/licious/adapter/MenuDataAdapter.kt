2d6f98a9621780a829f0254e0b68f8b5d31fb071
Abhishek <abhishekmaharajpet@gmail.com>
2020-07-13 00:56:44 +0530

    Implemented add to cart count

--- app/src/main/java/com/lib/licious/adapter/MenuDataAdapter.kt
+++ app/src/main/java/com/lib/licious/adapter/MenuDataAdapter.kt
@@ -2,12 +2,15 @@ package com.lib.licious.adapter
 
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.lib.licious.model.MenuDataModel
 import com.lib.licious.R
+import com.lib.licious.model.MenuDataModel
 import kotlinx.android.synthetic.main.item_meat_product.view.*
+import kotlinx.android.synthetic.main.layout_add_cart.view.*
 
 class MenuDataAdapter(private val menuDataModelList: List<MenuDataModel>?) : RecyclerView.Adapter<MenuDataAdapter.MenuViewHolder>() {
 
@@ -22,10 +25,43 @@ class MenuDataAdapter(private val menuDataModelList: List<MenuDataModel>?) : Rec
 
     override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
         holder.itemView.run {
+
             meatName.text = menuDataModelList?.get(position)?.name
             priceTxt.text = String.format("%s %d", "\u20B9", menuDataModelList?.get(position)?.price?.toInt())
-            weightText.text = String.format("%s %s","Net wt." , menuDataModelList?.get(position)?.weight)
+            weightText.text = String.format("%s %s", "Net wt.", menuDataModelList?.get(position)?.weight)
             Glide.with(this).load(menuDataModelList?.get(position)?.imageUrl).centerCrop().placeholder(R.drawable.placeholder_food).into(meatImage)
+            if (menuDataModelList?.get(position)?.count == 0) {
+                addCartLayout.visibility = GONE
+                addCartButton.visibility = VISIBLE
+            } else {
+                addCartLayout.visibility = VISIBLE
+                addCartButton.visibility = GONE
+                centerText.text = menuDataModelList?.get(position)?.count?.toString()
+            }
+            addCartButton.setOnClickListener {
+                addCartLayout.visibility = VISIBLE
+                addCartButton.visibility = GONE
+                centerText.text = "1"
+                menuDataModelList?.get(position)?.count = 1
+            }
+
+            plusTxt.setOnClickListener {
+                val int = centerText.text.toString().toInt()
+                centerText.text = (int + 1).toString()
+                menuDataModelList?.get(position)?.count = int + 1
+            }
+
+            minusTxt.setOnClickListener {
+                val int = centerText.text.toString().toInt()
+                if (int > 1) {
+                    centerText.text = (int - 1).toString()
+                } else {
+                    centerText.text = "1"
+                    addCartLayout.visibility = GONE
+                    addCartButton.visibility = VISIBLE
+                }
+                menuDataModelList?.get(position)?.count = int - 1
+            }
         }
     }
 
--- app/src/main/java/com/lib/licious/model/MenuDataModel.kt
+++ app/src/main/java/com/lib/licious/model/MenuDataModel.kt
@@ -1,3 +1,3 @@
 package com.lib.licious.model
 
-data class MenuDataModel(val imageUrl: String?, val name: String?, val weight: String?, val price: Double?)
+data class MenuDataModel(val imageUrl: String?, val name: String?, val weight: String?, val price: Double?, var count: Int = 0)
